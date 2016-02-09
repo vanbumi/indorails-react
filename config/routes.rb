adf7e3b50433b11dc06946b3e7f9ae4ab651ea34
vanbumi <jsp.dio@gmail.com>
2016-02-09 09:48:03 +0700

    create menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   get 'hello-world' => 'hello_world#index'
   
   get 'comments' => 'hello_world#comments'
@@ -10,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
